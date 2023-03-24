01fe72be76f2da2de3c30aacfde3a2e1d8cd498e
Matheus Kobayashi <matheusk2013@outlook.com>
2023-03-23 22:40:34 -0300

    first commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,66 @@
 import React from 'react';
+import CountUp from 'react-countup';
+import { useInView } from 'react-intersection-observer';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants';
 
 const About = () => {
-  return <div>About</div>;
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  })
+  return (
+    <section className='section' id='about' ref={ref}>
+      <div className='container mx-auto'>
+        <div className='flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen '>
+          <motion.div
+            variants={fadeIn('right', 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1 bg-about bg-contain bg-no-repeat h-[640px]
+          mix-blend-lighten bg-top'></motion.div>
+          <motion.div
+            variants={fadeIn('left', 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1'>
+            <h2 className='h2 text-accent'>Sobre mim</h2>
+            <h3 className='h3 mb-4'>
+              Eu sou desenvolvedor Front-end freelancer com 3 anos de experiência.
+            </h3>
+            <p className='mb-4'>
+              Estou sempre buscando aprender novas tecnologias e aprimorar minhas habilidades para oferecer as melhores soluções.
+            </p>
+            <div className='flex gap-x-6 lg:gap-x-10 mb-12'>
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                  {inView ? <CountUp start={0} end={3} duration={5} /> : null}
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>Anos de<br />Experiência</div>
+              </div>
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                  {inView ? <CountUp start={0} end={13} duration={5} /> : null}
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>Projetos<br />Completados</div>
+              </div>
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                  {inView ? <CountUp start={0} end={15} duration={5} /> : null}
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>Clientes<br />Satisfeitos</div>
+              </div>
+            </div>
+            <div className='flex gap-x-8 items-center'>
+              <button className='btn btn-lg'>Fale Comigo</button>
+              <a href='#' className='text-gradient btn-link'>Meu Portifólio</a>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </section >
+  );
 };
 
 export default About;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,57 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className='py-16 lg:section' id='contact'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col lg:flex-row'>
+          <motion.div
+            variants={fadeIn('left', 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1 flex jutify-start items-center'
+          >
+            <div>
+              <h4 className='text-xl uppercase text-accent font-medium mb-2 tracking-wide'>Entrar em contato</h4>
+              <h2 className='text-[45px] lg:text-[90px] leading-none mb-12'>
+                Vamos trabalhar <br />
+                juntos
+              </h2>
+            </div>
+          </motion.div>
+          <motion.form
+            variants={fadeIn('right', 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            style={{borderRadius: 20}}
+            className='flex-1 border flex flex-col gap-y-6 pb-24 p-6 items-start'>
+            <input
+              className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white 
+              focus:border-accent transition-all'
+              type="text"
+              placeholder='Seu nome'
+            />
+            <input
+              className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white 
+              focus:border-accent transition-all'
+              type="text"
+              placeholder='Seu email'
+            />
+            <textarea
+              className='bg-transparent border-b py-12 outline-none w-full placeholder:text-white 
+              focus:border-accent transition-all resize-none mb-12'
+              placeholder='Sua mensagem'
+            ></textarea>
+            <button className='btn btn-lg'>Enviar mensagem</button>
+          </motion.form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
